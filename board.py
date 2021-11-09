68c69625e809a56c7a74e7227d44dd7a01de9705
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-11-09 17:16:42 +0800

    Add method to the class board to assign values to the board

--- board.py
+++ board.py
@@ -27,3 +27,10 @@ class Board:
             board[row][column] = '*'
             bomb_planted +=1
         return board
+
+    def assign_values_to_board(self):
+        for r in range(self.dim_size):
+            for c in range(self.dim_size):
+                if self.board[r][c] == '*':
+                    continue
+                self.board[r][c] = self.get_num_neighboring_bombs(r, c)
